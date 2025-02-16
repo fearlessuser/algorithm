ab67ba005b88719533dcff4b2bffbeeb537e3825
zhangbenjing <209218780@qq.com>
2025-02-16 18:27:13 +0800

    first modify

---
+++ study2025_2_16/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"max.h"
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c;
+    scanf("%d",&c);
+    printf("%d\n",c);
+    printf("%d\n",max(a,b));
+    return 0 ;
+}
